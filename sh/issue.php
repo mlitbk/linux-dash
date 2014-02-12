08943d3fae5c20a24ecfbbcad475f98a14235ee5
Michael Litvak <mlitbk@gmail.com>
2014-02-12 16:17:15 +0200

    add commands to show hostname and os in osx

--- sh/issue.php
+++ sh/issue.php
@@ -1,3 +1,12 @@
 <?php 
 header('Content-Type: application/json; charset=UTF-8');
-echo json_encode(shell_exec('/usr/bin/lsb_release -ds;/bin/uname -r')) ;
+
+$output = shell_exec('/usr/bin/lsb_release -ds;/bin/uname -r');
+
+// for osx
+if(!$output && file_exists('/usr/bin/sw_vers')) {
+    $output = explode("\n",shell_exec('/usr/bin/sw_vers | /usr/bin/cut -f2-'));
+    $output = implode(" ", array($output[0], $output[1]));
+}
+
+echo json_encode($output);
--- sh/uptime.php
+++ sh/uptime.php
@@ -3,24 +3,37 @@
 header('Content-Type: application/json; charset=UTF-8'); 
 
 $totalSeconds = shell_exec("/usr/bin/cut -d. -f1 /proc/uptime");
-$totalMin   = $totalSeconds / 60;
-$totalHours = $totalMin / 60;
 
-$days  = floor($totalHours / 24);
-$hours = floor($totalHours - ($days * 24));
-$min   = floor($totalMin - ($days * 60 * 24) - ($hours * 60));
+// If we get error then try another command
+if(!$totalSeconds && file_exists('/usr/bin/uptime')) {
+    $output = shell_exec("/usr/bin/uptime | /usr/bin/awk '{print $3,$5}' | /usr/bin/cut -d, -f1");
+    $timeArray = explode(" ",$output);
+    $hoursArray = explode(":",$timeArray[1]);
+
+    $days = $timeArray[0];
+    $hours = $hoursArray[0];
+    $min = $hoursArray[1];
+} else {
+
+    $totalMin   = $totalSeconds / 60;
+    $totalHours = $totalMin / 60;
+
+    $days  = floor($totalHours / 24);
+    $hours = floor($totalHours - ($days * 24));
+    $min   = floor($totalMin - ($days * 60 * 24) - ($hours * 60));
+}
 
 $formatUptime = '';
 if ($days != 0) {
-	$formatUptime .= "$days days ";
+    $formatUptime .= "$days days ";
 }
 
 if ($hours != 0) {
-	$formatUptime .= "$hours hours ";
+    $formatUptime .= "$hours hours ";
 }  
 
 if ($min != 0) {
-	$formatUptime .= "$min minutes";
+    $formatUptime .= "$min minutes";
 } 
 
 echo json_encode($formatUptime);
